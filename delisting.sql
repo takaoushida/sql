4f8a2b766de10ab94802b710ce0eeee767560c7b
DESKTOP-BEQUOI4\User <ussissy11@yahoo.co.jp>
2025-12-23 20:00:46 +0900

    linuxいれるので

--- delisting.sql
+++ delisting.sql
@@ -9,20 +9,40 @@ for tables in(
             end_date
         from
             `stock_data_mst.delisting_20*`
-        where
-            end_date = current_date('Asia/Tokyo')
+    ),
+    delisting_dataset as(
+        select
+            distinct
+            _table_suffix as stock_code
+        from    
+            `stock_data_delisting.delisting_*`
+    ),
+    dataless_delisting as(
+        select  
+            t1.*
+        from    
+            delisting_mst as t1
+        left join
+            delisting_dataset as t2
+            on t1.stock_code = t2.stock_code
+        where   
+            t1.stock_code = t2.stock_code            
+            and t1.end_date >= date_add(current_date('Asia/Tokyo'),interval - 7 day)
+            and t2.stock_code is null
     ),
     tokyo_01 as(
         select
+            distinct
             _table_suffix as suffix_date
         from
             `stock_data.tokyo_01_*`
+        where parse_date('%Y%m%d',_table_suffix) >= date_add(current_date('Asia/Tokyo'),interval - 10 day)
     )
     select
         t1.stock_code,
         max(t2.suffix_date) as suffix
     from
-        delisting_mst as t1
+        dataless_delisting as t1
     inner join
         tokyo_01 as t2
         on t1.end_date >= parse_date('%Y%m%d',t2.suffix_date)
---
+++ dev_feature_learning.sql
@@ -0,0 +1,30 @@
+create or replace table feature_learning_dev.feature_learning
+partition by created_at
+cluster by stock_code as(
+with
+stock_data as(
+    select * from feature_learning_dev.stock_data_explanatory_valiable_add_20251203
+    where created_at >=  '2025-04-01'
+)
+select
+    t1.*,
+    t2.forcast_up_rate,
+    t3.forcast_down_rate,
+    t4.forcast_win_rate,
+    t5.forcast_lose_rate
+from
+    stock_data  as t1
+left join
+    feature_learning_dev.dev_feature_learning_up as t2
+    on t1.stock_code = t2.stock_code and t1.created_at = t2.created_at
+left join
+    feature_learning_dev.dev_feature_learning_down as t3
+    on t1.stock_code = t3.stock_code and t1.created_at = t3.created_at
+left join
+    feature_learning_dev.feature_learning_win as t4
+    on t1.stock_code = t4.stock_code and t1.created_at = t4.created_at
+left join
+    feature_learning_dev.feature_learning_lose as t5
+    on t1.stock_code = t5.stock_code and t1.created_at = t5.created_at
+)
+
