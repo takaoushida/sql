d2a898bcd17ec9f5af762e57418d061302dbddde
DESKTOP-BEQUOI4\User <ussissy11@yahoo.co.jp>
2026-01-02 21:41:38 +0900

    Lv２実装

--- feature_learning.sql
+++ feature_learning.sql
@@ -4,46 +4,27 @@ cluster by stock_code as(
 with
 stock_data as(
     select * from looker_datamart.stock_data_explanatory_valiable_add
-    where created_at >=  '2025-01-01'
-),
-later_close_tb as(
-    select
-        *,
-        max(created_at) over() as max_date
-    from
-        stock_data
-),
-latest_close_tb as(
-    select
-        *,
-    from
-        later_close_tb
-    where
-        created_at = max_date
+    where created_at >= '2016-06-01' 
 )
 select
     t1.*,
-    t2.close as latest_close,
-    t3.forcast_up_rate,
-    t4.forcast_down_rate,
-    t5.forcast_win_rate,
-    t6.forcast_lose_rate
+    t2.forcast_up_rate,
+    t3.forcast_down_rate,
+    t4.forcast_win_rate,
+    t5.forcast_lose_rate
 from
     stock_data  as t1
 left join
-    latest_close_tb as t2
-    on t1.stock_code = t2.stock_code    
+    temp_folder.feature_learning_up as t2
+    on t1.stock_code = t2.stock_code and t1.created_at = t2.created_at
 left join
-    looker_datamart.feature_learning_up as t3
+    temp_folder.feature_learning_down as t3
     on t1.stock_code = t3.stock_code and t1.created_at = t3.created_at
 left join
-    looker_datamart.feature_learning_down as t4
+    temp_folder.feature_learning_win as t4
     on t1.stock_code = t4.stock_code and t1.created_at = t4.created_at
 left join
-    looker_datamart.feature_learning_win as t5
+    temp_folder.feature_learning_lose as t5
     on t1.stock_code = t5.stock_code and t1.created_at = t5.created_at
-left join
-    looker_datamart.feature_learning_lose as t6
-    on t1.stock_code = t6.stock_code and t1.created_at = t6.created_at
 )
 
